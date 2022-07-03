6bf8ea0afb9e91644a392c000ee864db6a6ba0eb
gepiko <aoriaira@gmail.com>
2022-07-03 20:16:10 +0300

    checking if user is logged in

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
 
 import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import Paper from '@mui/material/Paper'
 import Button from '@mui/material/Button'
 import { useForm } from 'react-hook-form'
-import { fetchAuth } from '../../redux/slices/auth'
+import { fetchAuth, selectIsAuth } from '../../redux/slices/auth'
 
 import styles from './Login.module.scss'
 
 export const Login = () => {
+  const isAuth = useSelector(selectIsAuth)
   const dispatch = useDispatch()
+
   const {
     register,
     handleSubmit,
@@ -19,8 +22,8 @@ export const Login = () => {
     formState: { errors, isValid },
   } = useForm({
     defaultValues: {
-      email: 'test@test.ru',
-      password: '123',
+      email: '111test@test.ru',
+      password: '12345',
     },
     mode: 'onChange',
   })
@@ -29,7 +32,9 @@ export const Login = () => {
     dispatch(fetchAuth(values))
   }
 
-  console.log(errors, isValid)
+  if (isAuth) {
+    return <Navigate to='/' />
+  }
 
   return (
     <Paper classes={{ root: styles.root }}>
