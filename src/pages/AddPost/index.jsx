5b5b9093b96da7f4b7702e4779027fbbef112e17
gepiko <aoriaira@gmail.com>
2022-07-05 12:05:10 +0300

    sending post to backend

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Navigate } from 'react-router-dom'
+import { useNavigate, Navigate } from 'react-router-dom'
 
 import TextField from '@mui/material/TextField'
 import Paper from '@mui/material/Paper'
@@ -13,9 +13,10 @@ import 'easymde/dist/easymde.min.css'
 import styles from './AddPost.module.scss'
 
 export const AddPost = () => {
+  const navigate = useNavigate()
   const isAuth = useSelector(selectIsAuth)
   const [isLoading, setIsLoading] = React.useState(false)
-  const [value, setValue] = React.useState('')
+  const [text, setText] = React.useState('')
   const [title, setTitle] = React.useState('')
   const [tags, setTags] = React.useState('')
   const [imageUrl, setImageUrl] = React.useState('')
@@ -39,8 +40,29 @@ export const AddPost = () => {
   }
 
   const onChange = React.useCallback((value) => {
-    setValue(value)
+    setText(value)
   }, [])
+
+  const onSubmit = async () => {
+    try {
+      setIsLoading(true)
+
+      const fields = {
+        title,
+        imageUrl,
+        tags: tags.split(','),
+        text,
+      }
+
+      const { data } = await axios.post('/posts', fields)
+
+      const id = data._id
+
+      navigate(`/posts/${id}`)
+    } catch (error) {
+      console.warn('Ошибка при создании статьи')
+    }
+  }
 
   const options = React.useMemo(
     () => ({
@@ -93,9 +115,9 @@ export const AddPost = () => {
         onChange={(e) => setTags(e.target.value)}
         fullWidth
       />
-      <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
       <div className={styles.buttons}>
-        <Button size='large' variant='contained'>
+        <Button onClick={onSubmit} size='large' variant='contained'>
           Опубликовать
         </Button>
         <a href='/'>
